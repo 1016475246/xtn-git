6c32a3627bd326fad81362fe0834ac636e1810c4
1016475246 <1016475246@qq.com>
2019-05-11 09:28:28 +0800

    test

--- src/main/java/com/lanxin/App.java
+++ src/main/java/com/lanxin/App.java
@@ -27,7 +27,9 @@ public class App {
         //这是一行注解
         SpringApplication.run(App.class, args);
         //这是第二行注解
-        //这是第四行注解
+        
         //这是第三行注解
+
+        //这是第四行注解
     }
 }
